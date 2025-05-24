8d31ddc6f07d8629c7fcaa42f2f4649d6a1e18e5
AditiVish11 <vishwakarmaaditi599@gmail.com>
2025-05-24 09:08:01 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "aditi@gmail.com" 
+var accountPassword = "1111"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+
+
+accountEmail = "vish@gmail.com"
+accountPassword = "2211222"
+accountCity = "Benguluru" 
+console.log(accountId );
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer vevrsion
+
+// alert(3+3) //we are using node js , not browser
+
+console.log(3 + 3)
+// code readability should be high
+console.log("Aditi")
+
+let name = " Aditi"
+let age = 18
+let isLoggedIn = false // boolean dataType
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => True/False
+// null => standalone value => representation of empty value
+//undefined => abhi value assign nhi huyi hai
+//symbol=> unique
+
+
+//object
+
+
+
+console.log(typeof undefined) // output => undefined
+
+console.log(typeof null) // output =>  object
+
