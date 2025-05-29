a9c6512e6eae726bb44e063b6d9402b8490cf147
AditiVish11 <vishwakarmaaditi599@gmail.com>
2025-05-29 18:49:52 +0000

    Dates

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,14 @@
+// Dates 
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025, 0, 25)
+// let myCreatedDate = new Date(2025, 0, 25)
+
+let myCreatedDate = new Date("2025-01-21")
+console.log(myCreatedDate.toLocaleString());
+
+
