7d4d20bc28b93c2fe8f5b365d8b36a6abf4cc16f
AditiVish11 <vishwakarmaaditi599@gmail.com>
2025-06-10 18:47:18 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,66 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("February");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        // break;
+    case 4:
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
+const Days = "tuesday"
+switch (Days) {
+    case "Sun":
+        console.log("Sunday");
+        
+        break;
+    case "Mon":
+        console.log("Monday");
+        
+        break;
+    case "Tues":
+        console.log("Tuesday");
+        
+        break;
+    case "Wed":
+        console.log("Wednessday");
+        
+        break;
+    case "Thurs":
+        console.log("Thursday");
+        
+        break;
+    case "Fri":
+        console.log("Friday");
+        
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
--- 04_control_flow/two.js
+++
@@ -1,9 +0,0 @@
-// switch (key) {
-//     case value:
-        
-//         break;
-
-//     default:
-//         break;
-// }
-
