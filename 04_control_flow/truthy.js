5e32a971d996663fdc131dd6c60b5e50aea3546a
AditiVish11 <vishwakarmaaditi599@gmail.com>
2025-06-10 19:02:04 +0000

    truthy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,36 @@
+const userEmail = "Aditi.mail"
+
+if (userEmail) {
+    console.log("Got user email");
+    } else{
+        console.log("Don't have user email");
+        
+    }
+const userEmail2 = ""
+
+if (userEmail2) {
+    console.log("Got user email");
+    } else{
+        console.log("Don't have user email");
+        
+    }
+
+
+   // falsy value
+
+  // false, 0, -0, BigInt 0n,  "", null, undefined, NaN  ye sare falsy Value
+
+  //Truthy value 
+  // "0" , 'false' , " ", [ ], {}, function(){}
+
+
+//   if (userEmail.length === 0) {
+//     console.log("Array is empty");
+    
+//   }
+
+const emptyObj = {}
+if (Object.keys(emptyObj).length ===0) {
+    console.log("Object is empty");
+    
+}
