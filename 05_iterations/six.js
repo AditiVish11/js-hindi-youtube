130dfba8059d370a0e783e93a3157b8e5a101b1e
AditiVish11 <vishwakarmaaditi599@gmail.com>
2025-06-17 11:48:55 +0000

    map

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -59,5 +59,8 @@ const books = [
 
 let userBooks = books.filter( (bk) => bk.genre === 'History')
 
- userBooks = books.filter( (bk) =>  { return bk.publish >= 2000})
+userBooks = books.filter( (bk) =>  { 
+    return bk.publish >= 1995
+    && bk.genre === 'History'
+})
 console.log(userBooks);
